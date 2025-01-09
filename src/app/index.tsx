33411b7a07da8e48f0424d8500d6d7ebe3deaff8
Sergey Groshev <wer113wer@yandex.ru>
2025-01-09 14:34:36 +0300

    [feat](styles): create theme
    add manrope font to theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+import {ThemeProvider} from "@mui/material";
+import {theme} from "../shared/assets/theme";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </StrictMode>,
+);
