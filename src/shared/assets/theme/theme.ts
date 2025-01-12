6d1b04d2283474ae8ee150b111ce90a86f3c6ef9
Sergey Groshev <wer113wer@yandex.ru>
2025-01-12 20:03:11 +0300

    [style](theme): add primary color
    use in Gauge (Days Counter Card)

--- src/shared/assets/theme/theme.ts
+++ src/shared/assets/theme/theme.ts
@@ -3,5 +3,12 @@ import {createTheme} from "@mui/material";
 export const theme = createTheme({
     typography: {
         fontFamily: "Manrope, sans-serif",
-    }
+    },
+    palette: {
+        primary: {
+            main: "#3DCC6F",
+
+        }
+    },
+
 });
