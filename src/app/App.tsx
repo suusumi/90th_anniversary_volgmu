6162374c6a082714f3718d090650804befaf1c5f
Sergey Groshev <wer113wer@yandex.ru>
2025-01-13 22:21:42 +0300

    [feat](app.tsx): write base app structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
-import {Typography} from "@mui/material";
+import {Container, Typography} from "@mui/material";
+import {Header} from "../shared/header";
+import {MainPage} from "../pages/main";
+import {Footer} from "../shared/footer";
 
 function App() {
 
   return (
-    <>
-        <Typography>sdfok</Typography>
-    </>
+    <Container maxWidth="lg">
+        <Header/>
+        <MainPage/>
+        {/*<Footer/>*/}
+        {/*<Typography>sdfok</Typography>*/}
+    </Container>
   )
 }
 
