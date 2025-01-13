bb2c44fbf1b24af90d2e35324afb88df4708adfc
Sergey Groshev <wer113wer@yandex.ru>
2025-01-13 22:19:32 +0300

    [fix](daysCounter): fix component height, change Gauge size

--- src/features/daysCounter/ui/DaysCounter.tsx
+++ src/features/daysCounter/ui/DaysCounter.tsx
@@ -46,7 +46,6 @@ export const DaysCounter: React.FC = () => {
                     backgroundColor: "white",
                     borderRadius: "16px",
                     boxShadow: "0px 0px 200px rgba(0, 0, 0, 0.10)",
-                    maxWidth: "350px",
                 }}
             >
                 <Typography variant="h6">Загрузка данных...</Typography>
@@ -66,7 +65,6 @@ export const DaysCounter: React.FC = () => {
                 display: "flex",
                 alignItems: "center",
                 flexDirection: "column",
-                maxWidth: "350px",
                 boxShadow: "0px 0px 200px rgba(0, 0, 0, 0.10)",
             }}
         >
@@ -75,7 +73,6 @@ export const DaysCounter: React.FC = () => {
                     variant="h6"
                     sx={{
                         marginBottom: "15px",
-                        fontWeight: "bold",
                         position: "relative",
                         zIndex: 2, // Текст всегда поверх
                     }}
@@ -86,16 +83,16 @@ export const DaysCounter: React.FC = () => {
                     <Gauge
                         sx={{fontSize: "24px"}}
                         innerRadius="70%"
-                        width={200}
-                        height={200}
+                        width={140}
+                        height={140}
                         value={animatedValue}
                         valueMax={90}
                     />
                     <Typography
-                        variant="h4"
+                        variant="h6"
                         sx={{
                             position: "absolute",
-                            fontWeight: "bold",
+
                         }}
                     >
                         {Math.round(animatedValue)} {/* Тут Gauge показывает 11,012 (тысячные значения), поэтому скрыто значение Gauge и тут используется Math для округления */}
