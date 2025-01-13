b74f7368a6f35c12c3637fce7522b203ba6a767f
Sergey Groshev <wer113wer@yandex.ru>
2025-01-13 22:21:09 +0300

    [feat](project): install Regular and Bold Manrope font

--- src/shared/assets/theme/theme.ts
+++ src/shared/assets/theme/theme.ts
@@ -3,6 +3,8 @@ import {createTheme} from "@mui/material";
 export const theme = createTheme({
     typography: {
         fontFamily: "Manrope, sans-serif",
+        fontWeightRegular: 400, // Обычный текст
+        fontWeightBold: 700, // Жирный текст
     },
     palette: {
         primary: {
