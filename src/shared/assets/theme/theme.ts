e227a67852fa7714397b9eef65f1e9583a38569a
Sergey Groshev <wer113wer@yandex.ru>
2025-01-20 01:11:20 +0300

    [feat](theme): connect Austin Cyr font

--- src/shared/assets/theme/theme.ts
+++ src/shared/assets/theme/theme.ts
@@ -1,16 +1,15 @@
-import {createTheme} from "@mui/material";
+import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
     typography: {
         fontFamily: "Manrope, sans-serif",
-        fontWeightRegular: 400, // Обычный текст
-        fontWeightBold: 700, // Жирный текст
+        fontWeightRegular: 400,
+        fontWeightBold: 700,
+        body1: {fontFamily: "Austin Cyr"},
     },
     palette: {
         primary: {
             main: "#3DCC6F",
-
-        }
+        },
     },
-
 });
